5b7ea8e4c0d06b8c4be5a7848f34702a7fc9019d
MiraclePepple <miracle4424@gmail.com>
2025-11-16 23:06:34 +0100

    feat(users): implement user service with CRUD operations and password hashing

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepo: Repository<User>,
+  ) {}
+
+  async create(dto: CreateUserDto) {
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+
+    const user = this.usersRepo.create({
+      ...dto,
+      passwordHash,
+    });
+
+    return this.usersRepo.save(user);
+  }
+
+  findAll() {
+    return this.usersRepo.find();
+  }
+
+  async findOne(id: string) {
+    const user = await this.usersRepo.findOne({ where: { id } });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    return this.usersRepo.findOne({ where: { email } });
+  }
+
+  async update(id: string, dto: UpdateUserDto) {
+    const user = await this.findOne(id);
+
+    Object.assign(user, dto);
+    return this.usersRepo.save(user);
+  }
+
+  async remove(id: string) {
+    await this.findOne(id);
+    return this.usersRepo.delete(id);
+  }
+}
