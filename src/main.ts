443aae03ecc3e23fcbfaf1b5137400baf8ef1ac9
MiraclePepple <miracle4424@gmail.com>
2025-11-16 23:00:30 +0100

    feat: add Swagger documentation support with NestJS Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Smart Inventory API')
+    .setDescription('API documentation for the Smart Inventory and Sales system')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document); // Swagger URL → /api
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
